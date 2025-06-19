ccc9f4f9417731ad62e6a5c3c23e46f345d836f5
Avishka Chamod <avishkachamod14@gmail.com>
2025-06-20 00:19:10 +0530

    test

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -218,7 +218,7 @@ function History() {
                 Start New Quiz
               </button>
               <button className="history-button">
-                Export Data
+                Export 
               </button>
             </div>
           </div>
